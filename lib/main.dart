59a023cc41db4d8075e89075ecc7fcb309118ecf
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2022-05-09 02:47:57 +0100

    Setting Firebase.
    - Sey Firebase for Android and Web!

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:infinite/responsive/mobile_screen_layout.dart';
 import 'package:infinite/responsive/responsive_layout_screen.dart';
 import 'package:infinite/responsive/web_screen_layout.dart';
 import 'package:infinite/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if(kIsWeb) {
+await Firebase.initializeApp(
+  options: const FirebaseOptions(
+    apiKey: "AIzaSyCXoItONnadh5dQGOuVCLk1hPIc0lXFwKY",
+   appId: "1:836310769366:web:c3c660088779871454b377",
+    messagingSenderId: "836310769366"
+   , projectId: "infinite-7e617",
+   storageBucket: "infinite-7e617.appspot.com"
+   )
+);
+  }
+  else {
+await Firebase.initializeApp();
+  }
+  
   runApp(const MyApp());
 }
 
@@ -22,9 +40,8 @@ class MyApp extends StatelessWidget {
       ),
       home: const ResponsiveLayout(
         mobileScreenLayout: MobileScreenLayout(),
-         webScreenLayout: WebScreenLayout(),
-         ),
+        webScreenLayout: WebScreenLayout(),
+      ),
     );
   }
 }
-
