eb7e4873d561dafa3d41eda06fbbd000f69b5f2d
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2022-05-13 05:44:53 +0100

    LoginScreen(UI)
    - Done from creating the UI-LoginScreen.
    *Note: Buttons not clickable, not functional input yet.

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:infinite/responsive/mobile_screen_layout.dart';
 import 'package:infinite/responsive/responsive_layout_screen.dart';
 import 'package:infinite/responsive/web_screen_layout.dart';
+import 'package:infinite/screens/login_screen.dart';
 import 'package:infinite/utils/colors.dart';
 
 void main() async {
@@ -38,10 +39,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: mobileBackgroundColor,
       ),
-      home: const ResponsiveLayout(
-        mobileScreenLayout: MobileScreenLayout(),
-        webScreenLayout: WebScreenLayout(),
-      ),
+      // home: const ResponsiveLayout(
+      //   mobileScreenLayout: MobileScreenLayout(),
+      //   webScreenLayout: WebScreenLayout(),
+      // ),
+      home: LoginScreen(),
     );
   }
 }
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:infinite/utils/colors.dart';
+import 'package:infinite/widgets/text_field_input.dart';
+
+class LoginScreen extends StatefulWidget {
+  LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding:const  EdgeInsets.symmetric(horizontal: 32),
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Flexible(child: Container(), flex: 2),
+              // SVG Image
+              SvgPicture.asset
+              ("assets/log.svg",
+               color: primaryColor,
+                height: 64,
+                ),
+               const SizedBox(height: 64),
+              // Text Field For (Email)
+              TextFieldInput(
+                hintText: "Enter Your Email..",
+                textInputType: TextInputType.emailAddress,
+                textEditingController: emailController,
+              ),
+               const SizedBox(height: 32,),
+               TextFieldInput(
+                 hintText: "Enter Your Password..",
+                 textInputType: TextInputType.visiblePassword,
+                 textEditingController: passwordController,
+                 isPass: true,
+               ),
+              //Button Login
+               const SizedBox(height: 32,),
+              Container(
+                child: const Text('Log In'),
+                width: double.infinity,
+                alignment: Alignment.center,
+                padding: const EdgeInsets.symmetric(vertical: 12),
+                decoration: const ShapeDecoration(shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all
+                  (Radius.circular(4),
+                  ),
+                ),
+               
+                color: goldColor,
+                ),
+              ),
+              const SizedBox(height: 12,),
+              //Transitioning to Signup.
+              Flexible(child: Container(), flex: 2),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    child: const Text("New Here?"),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 8,
+                      ),
+
+                  ),
+                     Container(
+                    child: const Text("Sign Up!", style: TextStyle(fontWeight: FontWeight.bold,),),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 8,
+                      ),
+
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
